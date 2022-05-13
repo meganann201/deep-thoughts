12641d5edc37778d7adb2543f1f196f4f9ffb75a
Megan Campbell <megancampbell201@gmail.com>
2022-05-13 13:57:25 -0400

    create thought and reaction front end

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,8 +1,13 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { useQuery } from "@apollo/client";
-import { QUERY_THOUGHT } from "../utils/queries";
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
 import ReactionList from '../components/ReactionList';
+import ReactionForm from '../components/ReactionForm';
+
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHT } from '../utils/queries';
+
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -31,6 +36,7 @@ const SingleThought = (props) => {
     </div>
   </div>
   {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
+  {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
 </div>
   );
 };
